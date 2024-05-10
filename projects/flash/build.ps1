7f010d454137ed903be202f8b92b335c1454e93c
Kevin <zhouyan122488@163.com>
2024-05-10 14:05:57 +0800

    fix(build-script): 1. modify build.ps1 and stm32f1xx_hal_conf.h

--- projects/flash/build.ps1
+++ projects/flash/build.ps1
@@ -1,5 +1,7 @@
 # config env variable
-./path_helper.ps1
+$env:STM32_TOOLCHAIN_PATH="D:\toolchains\arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi"
+$env:STM32_TARGET_TRIPLET="arm-none-eabi"
+$env:STM32_CUBE_F1_PATH="..\..\stm32cube\STM32CubeF1-1.8.5"
 
 # Set build directory path
 $BuildPath = "../../build/projects/flash"
@@ -12,6 +14,32 @@ if (Test-Path -Path $BuildPath) {
 else {
     mkdir -p $BuildPath
 }
+
+# defibe function
+function buildClangdConfig {
+    [CmdletBinding()]
+    param (
+        [string]$ConfigFilePath, 
+        [string]$ConfigContent
+    )
+
+    $FileExist = Test-Path -Path $ConfigFilePath
+    if(-not ($FileExist))
+    {
+        New-Item $ConfigFilePath
+    }
+    Set-Content $ConfigFilePath -value $ConfigContent
+}
+
+# set .clangd for clangd server
+$CurrentClangdConfigFile = ".clangd"
+$STM32CubeClangdConfigFile = "../../stm32cube/STM32CubeF1-1.8.5/.clangd"
+$ClangdConfigContent = "CompileFlags:
+  CompilationDatabase: >-
+    $BuildPath";
+
+buildClangdConfig -ConfigFilePath $CurrentClangdConfigFile -ConfigContent $ClangdConfigContent
+buildClangdConfig -ConfigFilePath $STM32CubeClangdConfigFile -ConfigContent $ClangdConfigContent
 
 # Get current source directiory
 $CurrentPath = Get-Location
--- projects/flash/path_helper.ps1
+++
@@ -1,3 +0,0 @@
-$env:STM32_TOOLCHAIN_PATH="D:\toolchains\arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi"
-$env:STM32_TARGET_TRIPLET="arm-none-eabi"
-$env:STM32_CUBE_F1_PATH="E:\project\stm32\stm32-cmake-2.1.0\stm32cube\STM32CubeF1-1.8.5"
